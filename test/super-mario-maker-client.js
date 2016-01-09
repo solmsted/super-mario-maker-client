75b0129fe3bd44c96347e1d9740a5bf4c4fc2ad4
solmsted <steven.olm@gmail.com>
2016-01-09 15:31:00 -0500

    Moved static course object to its own module.

--- test/super-mario-maker-client.js
+++ test/super-mario-maker-client.js
@@ -16,6 +16,8 @@ import {
     join
 } from 'path';
 
+import booYall from './js/boo-yall.js';
+
 import expectCourse from './js/expect-course.js';
 
 import SuperMarioMakerClient, {
@@ -72,109 +74,7 @@ describe('SuperMarioMakerClient', function () {
                     return;
                 }
 
-                expectCourse(course, {
-                    attempts: 34,
-                    clearedBy: [{
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/pvvu3jjpnnd_normal_face.png',
-                        miiName: 'Jay'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/2lt69vsve0ct7_normal_face.png',
-                        miiName: 'Gilbes'
-                    }],
-                    clearRate: 5.88,
-                    clears: 2,
-                    courseId: 'DA56-0000-014A-DA36',
-                    createdAt: '7 days ago',
-                    creator: {
-                        country: 'US',
-                        medals: 2,
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/3bz2qs0l93czd_normal_face.png',
-                        miiName: 'Steven'
-                    },
-                    csrfToken: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
-                    difficulty: 'expert',
-                    firstClearBy: {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/2lt69vsve0ct7_normal_face.png',
-                        miiName: 'Gilbes'
-                    },
-                    gameStyle: 'superMarioBros3',
-                    imageUrl: 'https://dypqnhofrd2x2.cloudfront.net/DA56-0000-014A-DA36_full.jpg',
-                    miiversePostUrl: 'https://miiverse.nintendo.net/posts/AYMHAAACAAADVHkvlkvobA',
-                    players: 12,
-                    recentPlayers: [{
-                        country: 'JP',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/wcqya9850e6y_normal_face.png',
-                        miiName: 'いっしー'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/3ablfalo6lahl_normal_face.png',
-                        miiName: 'RJ'
-                    }, {
-                        country: 'CA',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/32mb4xqiqnz29_normal_face.png',
-                        miiName: 'Chris'
-                    }, {
-                        country: 'JP',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/1hnuppn53vbay_normal_face.png',
-                        miiName: 'ゆうな'
-                    }, {
-                        country: 'JP',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/1s36j4j6a49s3_normal_face.png',
-                        miiName: 'みいー'
-                    }, {
-                        country: 'FR',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/19cae4ogyt99u_normal_face.png',
-                        miiName: 'Jonathan'
-                    }, {
-                        country: 'CA',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/5romhgvhjog2_normal_face.png',
-                        miiName: 'devon'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/pvvu3jjpnnd_normal_face.png',
-                        miiName: 'Jay'
-                    }, {
-                        country: 'DE',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/y0kt3vkt3sz1_normal_face.png',
-                        miiName: 'Marlon'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/2lt69vsve0ct7_normal_face.png',
-                        miiName: 'Gilbes'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/3s10roz55c3kt_normal_face.png',
-                        miiName: 'Dεεst'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/3inwjii9bud94_normal_face.png',
-                        miiName: 'Dylan M'
-                    }],
-                    starredBy: [{
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/pvvu3jjpnnd_normal_face.png',
-                        miiName: 'Jay'
-                    }, {
-                        country: 'US',
-                        miiIconUrl: 'http://mii-images.cdn.nintendo.net/2lt69vsve0ct7_normal_face.png',
-                        miiName: 'Gilbes'
-                    }],
-                    stars: 2,
-                    thumbnailUrl: 'https://dypqnhofrd2x2.cloudfront.net/DA56-0000-014A-DA36.jpg',
-                    title: 'Boo Y\'all v2',
-                    tweets: 0,
-                    worldRecord: {
-                        player: {
-                            country: 'US',
-                            miiIconUrl: 'http://mii-images.cdn.nintendo.net/2lt69vsve0ct7_normal_face.png',
-                            miiName: 'Gilbes'
-                        },
-                        time: 705900
-                    }
-                });
+                expectCourse(course, booYall);
 
                 mockedRequests.done();
 
